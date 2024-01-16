a04d1d352efa7f68541e816764aaa6ee5a10cbf5
hengage <tunecarterkillz@gmail.com>
2024-01-16 12:40:37 +0100

    Feature: check catgeory name exists

--- api/features/adminOperations/services/products.services.ts
+++ api/features/adminOperations/services/products.services.ts
@@ -1,7 +1,20 @@
+import { HandleException, STATUS_CODES } from "../../../utils";
 import { ProductCategory } from "../../products";
 
 class AdminOpsForProductsService {
   async createCategory(payload: any) {
+    const categoryExists = await ProductCategory.findOne({ name: payload.name })
+      .select("name")
+      .lean()
+      .exec();
+
+    if (categoryExists) {
+      throw new HandleException(
+        STATUS_CODES.CONFLICT,
+        "The category name already exists"
+      );
+    }
+
     const category = await ProductCategory.create({
       name: payload.name,
     });
