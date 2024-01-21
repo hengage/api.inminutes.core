aa9e4276f071033d2c10b9a5efe3082f11a09aef
hengage <tunecarterkillz@gmail.com>
2024-01-21 15:25:58 +0100

    Feature: reduce product quantity

--- api/features/products/repository/products.repo.ts
+++ api/features/products/repository/products.repo.ts
@@ -1,17 +1,10 @@
+import { HandleException, STATUS_CODES } from "../../../utils";
 import { Product } from "../models/products.models";
 
 class ProductsRepository {
   async addProduct(payload: any, vendor: string) {
-    const {
-      name,
-      image,
-      description,
-      quantity,
-      cost,
-      tags,
-      addOns,
-      category,
-    } = payload;
+    const { name, image, description, quantity, cost, tags, addOns, category } =
+      payload;
     const product = await Product.create({
       name,
       image,
@@ -31,8 +24,17 @@ class ProductsRepository {
       description: product.description,
       quantity: product.quantity,
       cost: product.cost,
-      addOns: product.addOns
+      addOns: product.addOns,
     };
+  }
+
+  async decreaseproductQuantity(productId: string, quantity: number) {
+    const product = await Product.findById(productId);
+    if (!product) {
+      throw new HandleException(STATUS_CODES.NOT_FOUND, "Product not found");
+    }
+    product.quantity -= quantity;
+    await product.save();
   }
 }
 
