49e9b7920784493601eab294321813152711d2e2
hengage <tunecarterkillz@gmail.com>
2024-03-20 13:34:10 +0100

    Feature: validate create order

--- api/features/orders/controllers/orders.controller.ts
+++ api/features/orders/controllers/orders.controller.ts
@@ -3,12 +3,14 @@ import { Request, Response } from "express";
 import { STATUS_CODES, handleErrorResponse } from "../../../utils";
 import { orderRepo } from "../repository/orders.repo";
 import { ordersService } from "../services/orders.service";
+import { validateOrders } from "../validation/orders.validation";
 
 class OrderController {
   async create(req: Request, res: Response) {
     const customer = (req as any).user._id;
 
     try {
+      await validateOrders.create(req.body)
       const order = await  ordersService.create({payload: req.body, customer})
 
       res.status(STATUS_CODES.CREATED).json({
--- api/features/orders/validation/orders.validation.ts
+++ api/features/orders/validation/orders.validation.ts
@@ -2,10 +2,39 @@ import Joi from "joi";
 import { HandleException, STATUS_CODES } from "../../../utils";
 
 class ValidateOrders {
-  assignRider = async (payload: {
-    orderId: string;
-    riderId: string;
-  }) => {
+  create = async (payload: any) => {
+    const schema = Joi.object({
+      recipientPhoneNumber: Joi.string()
+        .label("Recipient's phone number")
+        .required(),
+      items: Joi.array().required(),
+      vendor: Joi.string().label("Vendor").required(),
+      deliveryAddress: Joi.string().label("Delivery address").required(),
+      deliveryLocation: Joi.array().label("Delivery location").required(),
+      deliveryFee: Joi.string().label("Delivery fee").required(),
+      totalProductsCost: Joi.string().label("Total products cost").required(),
+      totalCost: Joi.string().label("Total cost").required(),
+      instruction: Joi.string().label("Instruction"),
+      type: Joi.string().label("Order type").required(),
+      scheduledDeliveryTime: Joi.string().when("type", {
+        is: "scheduled",
+        then: Joi.string().label("Scheduled delivery time").required(),
+        otherwise: Joi.forbidden(),
+      }),
+    });
+
+    const { error } = schema.validate(payload, {
+      allowUnknown: false,
+      abortEarly: false,
+    });
+
+    if (error) {
+      throw new HandleException(STATUS_CODES.BAD_REQUEST, error.message);
+    }
+    return;
+  };
+
+  assignRider = async (payload: { orderId: string; riderId: string }) => {
     const schema = Joi.object({
       orderId: Joi.string().label("Order id").required(),
       riderId: Joi.string().label("Rider id").required(),
