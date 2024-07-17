5fdcda420e2a7a92c40895912917b28278a03830
hengage <tunecarterkillz@gmail.com>
2024-07-17 12:56:38 +0100

    Feature: errand schema

---
+++ api/features/errand/errand.interface.ts
@@ -0,0 +1,36 @@
+import { Document } from "mongoose";
+import { ICustomerDocument } from "../customers";
+import { IRiderDocument } from "../riders";
+import { ErrandStatus } from "../../utils/constants.utils";
+
+export interface IErrandDocument extends Document {
+  _id: string;
+  packageType: [IErrandPackageTypeDocument];
+  customer: ICustomerDocument;
+  rider: IRiderDocument["_id"];
+  description: string;
+  pickupAddress: string;
+  pickupCoordinates: {
+    type: string;
+    coordinates: [number, number];
+  };
+  dropoffAddress: string;
+  dropoffCoordinates: {
+    type: string;
+    coordinates: [number, number];
+  };
+  dispatchFee: string;
+  status: ErrandStatus;
+  type: "instant" | "scheduled";
+  scheduledPickUpTime?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IErrandPackageTypeDocument extends Document {
+  _id: string;
+  packageType: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+}
