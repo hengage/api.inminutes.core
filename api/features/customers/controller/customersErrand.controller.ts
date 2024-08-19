cc71ff61d53f6e9d918852825d98e587959d99a0
hengage <tunecarterkillz@gmail.com>
2024-08-19 15:13:00 +0100

    fix: undefined error on customers errand service

--- api/features/customers/controller/customersErrand.controller.ts
+++ api/features/customers/controller/customersErrand.controller.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from "express";
 import { CustomerErrandService } from "../services/customerErrand.service";
 import { handleErrorResponse, STATUS_CODES } from "../../../utils";
-import { ValidateCustomer } from "../validators/customers.validator";
 
 export class CustomerErrandController {
   private customerErrandService: CustomerErrandService;
-  private validateCustomer: ValidateCustomer;
   constructor() {
     this.customerErrandService = new CustomerErrandService();
-    this.validateCustomer = new ValidateCustomer();
   }
 
-  async getHistory(req: Request, res: Response) {
-    const userType = req.query.usertype as "customer" ;
+  getHistory = async (req: Request, res: Response) => {
+    const userType = req.query.usertype as "customer";
     const page = parseInt(req.query.page as string);
     const limit = parseInt(req.query.limit as string);
     const customerId = (req as any).user._id;
@@ -20,7 +17,6 @@ export class CustomerErrandController {
     console.log({ userType, customerId, page, limit });
 
     try {
-    //   await this.validateCustomer.errandHistory(userType, res);
       const history = await this.customerErrandService.getHistory({
         userType,
         customerId,
@@ -34,5 +30,5 @@ export class CustomerErrandController {
     } catch (error: any) {
       handleErrorResponse(res, error);
     }
-  }
+  };
 }
