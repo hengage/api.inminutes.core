767625e7670ed2a1df97e8a54a68481b9ba3e01c
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2025-03-11 15:26:52 +0100

    feat(admin_rider): create ride, update ride and get rider wallet

--- api/features/adminOperations/controllers/riders.controller.ts
+++ api/features/adminOperations/controllers/riders.controller.ts
@@ -1,6 +1,8 @@
 import { ACCOUNT_STATUS, HTTP_STATUS_CODES } from "../../../constants";
 import { handleErrorResponse } from "../../../utils";
 import { handleSuccessResponse } from "../../../utils/response.utils";
+import { ridersService } from "../../riders";
+import { validateRider } from "../../riders/validators/riders.validators";
 import { adminOpsRidersService } from "../services/riders.service";
 import { Request, Response } from "express";
 
@@ -68,6 +70,40 @@ export const adminOpsRidersController: AdminOpsRidersController = {
       res.status(statusCode).json(errorJSON);
     }
   },
+
+  async createRider(req: Request, res: Response): Promise<void> {
+    try {
+      await validateRider.signUp(req.body);
+      const rider = await ridersService.signup(req.body);
+
+      handleSuccessResponse(
+        res,
+        HTTP_STATUS_CODES.CREATED,
+        rider,
+        "Rider account created",
+      );
+    } catch (error: any) {
+      console.error("Error signing up rider:", error);
+      const { statusCode, errorJSON } = handleErrorResponse(error);
+      res.status(statusCode).json(errorJSON);
+    }
+  },
+
+  async updateRider(req: Request, res: Response): Promise<void> {
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+
+  async getRiderWallet(req: Request, res: Response): Promise<void> {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
 };
 
 interface AdminOpsRidersController {
@@ -75,4 +111,7 @@ interface AdminOpsRidersController {
   riderDetails(req: Request, res: Response): Promise<void>;
   setAccountStatus(req: Request, res: Response): Promise<void>;
   setApprovalStatus(req: Request, res: Response): Promise<void>;
+  createRider(req: Request, res: Response): Promise<void>;
+  updateRider(req: Request, res: Response): Promise<void>;
+  getRiderWallet(req: Request, res: Response): Promise<void>;
 }
