6792dcbb08827d89209c14737dab68ca96cc33fc
hengage <henrychizobaudeh@gmail.com>
2025-06-15 03:45:50 +0100

    feat(admin operations): refactor products
    - filtering and validation to use ListProductsQueryParams,
    - fix linter type errors

--- api/features/adminOperations/interfaces/product.interface.ts
+++ api/features/adminOperations/interfaces/product.interface.ts
@@ -1,37 +1,42 @@
 import { SORT_ORDER } from "../../../constants";
-import { IProductCategoryDocument, IProductSubCategoryDocument } from "../../products/products.interface";
+import {
+  IProductCategoryDocument,
+  IProductSubCategoryDocument,
+} from "../../products/products.interface";
 
 export interface ProductSummaryResponse {
-    totalProducts: number;
-    newProducts: number;
-    returningProducts: any;
+  totalProducts: number;
+  newProducts: number;
+  returningProducts: any;
 }
 
 export interface GetProductRangeFilter {
-    startDate?: string;
-    endDate?: string;
+  startDate?: string;
+  endDate?: string;
 }
 
-export interface GetProductsFilter {
-    searchQuery?: string;
-    fromDate?: string;
-    toDate?: string;
-    category?: string;
-    vendor?: string;
-    status?: string;
-    maxPrice?: string;
-    minPrice?: string;
-    sort?: SORT_ORDER;
-  }
+export interface ListProductsQueryParams {
+  searchQuery?: string;
+  fromDate?: string;
+  toDate?: string;
+  category?: string;
+  vendor?: string;
+  status?: string;
+  maxPrice?: string;
+  minPrice?: string;
+  sortOrder?: SORT_ORDER;
+  page?: number | string;
+  limit?: number | string;
+}
 
 export interface GetCategoriesQuery {
-    searchQuery?: string;
-    page: number;
-    limit: number;
-  }
+  searchQuery?: string;
+  page: number;
+  limit: number;
+}
 
 export interface CategorySubCategoriesResponse {
-    subCategories: IProductSubCategoryDocument['name' | 'id'][];
-    totalSubCategories: number;
-    category: IProductCategoryDocument['name' | 'id'];
+  subCategories: IProductSubCategoryDocument["name" | "id"][];
+  totalSubCategories: number;
+  category: IProductCategoryDocument["name" | "id"];
 }
--- api/features/adminOperations/validators/products.validate.ts
+++ api/features/adminOperations/validators/products.validate.ts
@@ -2,100 +2,113 @@ import joi from "joi";
 import { Msg } from "../../../utils";
 import { SORT_ORDER } from "../../../constants";
 import { validateSchema } from "../../../utils/validation.utils";
-import { GetProductsFilter } from "../interfaces/product.interface";
+import { ListProductsQueryParams } from "../interfaces/product.interface";
 
 export class ValidateAdminOpsProducts {
-    static getList = async (getListData: GetProductsFilter & { page: number | string } & { limit: number | string } ) => {
-        const schema = joi.object({
-            page: joi.alternatives().try(
-                joi.number().integer().min(1),
-                joi.string().min(1)
-            ).optional().label("Page"),
-            limit: joi.alternatives().try(
-                joi.number().integer().min(1),
-                joi.string().min(1)
-            ).optional().label("Limit"),
-            searchQuery: joi.string().optional().allow('').label("Search Query"),
-            fromDate: joi.string().optional().allow('').label("From Date"),
-            toDate: joi.string().optional().allow('').label("To Date"),
-            maxPrice: joi.string().optional().allow('').label("Max Price"),
-            minPrice: joi.string().optional().allow('').label("Min Price"),
-            status: joi.string().optional().allow('').label("Status"),
-            category: joi.string().optional().allow("").label("Category"),
-            subCategory: joi.string().optional().allow("").label("Sub Category"),
-            vendor: joi.string().optional().allow("").label("Vendor"),
-            sort: joi.string().valid(...Object.values(SORT_ORDER)).label("Sort")
-                .messages({ "any.only": Msg.ERROR_INVALID_SORT_ORDER() }),
-        });
+  static getList = async (getListData: ListProductsQueryParams) => {
+    const schema = joi.object({
+      page: joi
+        .alternatives()
+        .try(joi.number().integer().min(1), joi.string().min(1))
+        .optional()
+        .label("Page"),
+      limit: joi
+        .alternatives()
+        .try(joi.number().integer().min(1), joi.string().min(1))
+        .optional()
+        .label("Limit"),
+      searchQuery: joi.string().optional(),
+      fromDate: joi.string().optional(),
+      toDate: joi.string().optional(),
+      maxPrice: joi.string().optional(),
+      minPrice: joi.string().optional(),
+      status: joi.string().optional(),
+      category: joi.string().optional(),
+      subCategory: joi.string().optional(),
+      vendor: joi.string().optional(),
+      sortOrder: joi
+        .string()
+        .valid(...Object.values(SORT_ORDER))
+        .messages({ "any.only": Msg.ERROR_INVALID_SORT_ORDER() }),
+    });
 
-        validateSchema(schema, getListData);
-        return;
-    };
+    validateSchema(schema, getListData);
+    return;
+  };
 
-    static getProductDetails = async (getProductDetailsData: { productId: string }) => {
-        const schema = joi.object({
-            productId: joi.string().required().label("Product ID"),
-        });
+  static getProductDetails = async (getProductDetailsData: {
+    productId: string;
+  }) => {
+    const schema = joi.object({
+      productId: joi.string().required().label("Product ID"),
+    });
 
-        validateSchema(schema, getProductDetailsData);
-        return;
-    };
+    validateSchema(schema, getProductDetailsData);
+    return;
+  };
 
-    static createCategory = async (createCategoryData: { name: string }) => {
-        const schema = joi.object({
-            name: joi.string().required().label("Name"),
-        });
+  static createCategory = async (createCategoryData: { name: string }) => {
+    const schema = joi.object({
+      name: joi.string().required().label("Name"),
+    });
 
-        validateSchema(schema, createCategoryData);
-        return;
-    };
+    validateSchema(schema, createCategoryData);
+    return;
+  };
 
-    static createSubCategory = async (createSubCategoryData: { name: string; category: string }) => {
-        const schema = joi.object({
-            name: joi.string()
-            .required()
-            .messages({
-                "any.required": Msg.ERROR_REQUIRED("name"),
-            }),
-            category: joi.string()
-            .required()
-            .messages({
-                "any.required": Msg.ERROR_REQUIRED("category"),
-            }),
-        });
+  static createSubCategory = async (createSubCategoryData: {
+    name: string;
+    category: string;
+  }) => {
+    const schema = joi.object({
+      name: joi
+        .string()
+        .required()
+        .messages({
+          "any.required": Msg.ERROR_REQUIRED("name"),
+        }),
+      category: joi
+        .string()
+        .required()
+        .messages({
+          "any.required": Msg.ERROR_REQUIRED("category"),
+        }),
+    });
 
-        validateSchema(schema, createSubCategoryData);
-        return;
-    };
+    validateSchema(schema, createSubCategoryData);
+    return;
+  };
 
-    static approveProduct = async (approveProductData: { productId: string }) => {
-        const schema = joi.object({
-            productId: joi.string().required().label("Product ID"),
-        });
+  static approveProduct = async (approveProductData: { productId: string }) => {
+    const schema = joi.object({
+      productId: joi.string().required().label("Product ID"),
+    });
 
-        validateSchema(schema, approveProductData);
-        return;
-    };
+    validateSchema(schema, approveProductData);
+    return;
+  };
 
-    static rejectProduct = async (rejectProductData: { productId: string }) => {
-        const schema = joi.object({
-            productId: joi.string().required().label("Product ID"),
-        });
+  static rejectProduct = async (rejectProductData: { productId: string }) => {
+    const schema = joi.object({
+      productId: joi.string().required().label("Product ID"),
+    });
 
-        validateSchema(schema, rejectProductData);
-        return;
-    };
+    validateSchema(schema, rejectProductData);
+    return;
+  };
 
-    static pendingProducts = async (pendingProductsData: { page: number | string }) => {
-        const schema = joi.object({
-            page: joi.alternatives().try(
-                joi.number().integer().min(1),
-                joi.string().min(1)
-            ).optional().label("Page"),
-        });
+  static pendingProducts = async (pendingProductsData: {
+    page: number | string;
+  }) => {
+    const schema = joi.object({
+      page: joi
+        .alternatives()
+        .try(joi.number().integer().min(1), joi.string().min(1))
+        .optional()
+        .label("Page"),
+    });
 
-        validateSchema(schema, pendingProductsData);
-        return;
-    };
+    validateSchema(schema, pendingProductsData);
+    return;
+  };
 }
-
