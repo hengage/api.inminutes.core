b79bed2ac0198923f5b96b4d4838f424a8778433
hengage <henrychizobaudeh@gmail.com>
2025-06-15 22:18:17 +0100

    chore: Adds error logging and improves consistency in response handling
    Enhances error handling by logging errors to aid debugging

--- api/utils/response.utils.ts
+++ api/utils/response.utils.ts
@@ -7,7 +7,7 @@ import { HandleException } from "./handleException.utils";
 export function createErrorResponse(
   code: ErrorCode,
   message: string,
-  details?: Record<string, unknown>,
+  details?: Record<string, unknown>
 ): ApiError {
   return {
     status: "error",
@@ -24,16 +24,18 @@ export function handleErrorResponse(err: unknown): {
   statusCode: number;
   errorJSON: ApiError;
 } {
+  console.log("Error occurred:", err);
+
   if (err instanceof HandleException) {
     const errorCode = Object.entries(HTTP_STATUS_CODES).find(
-      ([, code]) => code === err.status,
+      ([, code]) => code === err.status
     )?.[0] as ErrorCode;
 
     return {
       statusCode: err.status,
       errorJSON: createErrorResponse(
         errorCode || "INTERNAL_SERVER_ERROR",
-        err.message,
+        err.message
       ),
     };
   } else if (err instanceof Error) {
@@ -42,7 +44,7 @@ export function handleErrorResponse(err: unknown): {
       statusCode: HTTP_STATUS_CODES.SERVER_ERROR,
       errorJSON: createErrorResponse(
         "SERVER_ERROR",
-        "An unexpected error occurred",
+        "An unexpected error occurred"
       ),
     };
   } else {
@@ -51,7 +53,7 @@ export function handleErrorResponse(err: unknown): {
       statusCode: HTTP_STATUS_CODES.SERVER_ERROR,
       errorJSON: createErrorResponse(
         "SERVER_ERROR",
-        "An unknown error occurred",
+        "An unknown error occurred"
       ),
     };
   }
@@ -59,7 +61,7 @@ export function handleErrorResponse(err: unknown): {
 
 export function createSuccessResponse<T = JSONValue | null>(
   data: T,
-  message?: string,
+  message?: string
 ): ApiSuccessResponse<T> {
   return {
     status: "success",
@@ -71,11 +73,11 @@ export function handleSuccessResponse<T = JSONValue | null>(
   res: Response,
   statusCode: number,
   data: T,
-  message?: string,
+  message?: string
 ) {
   const successResponse: ApiSuccessResponse<T> = createSuccessResponse(
     data,
-    message,
+    message
   );
   res.status(statusCode).json(successResponse);
 }
